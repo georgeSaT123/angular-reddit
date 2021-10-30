0719b0617e4e119e00cc8e0722d6ee9293ceee46
georgeSaT123 <76953557+georgeSaT123@users.noreply.github.com>
2021-10-29 20:30:39 -0500

    ruta

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/primerLaboratorio'));
+app.use(express.static('./primerLaboratorio'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/primerLaboratorio/'}),
+    res.sendFile('index.html', {root: '/primerLaboratorio/'}),
 );
 
 // Start the app by listening on the default Heroku port
